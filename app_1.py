1be91b94900521f5f04be5729ce4bce7264b02d0
Administrator <admin@example.com>
2018-06-07 13:00:07 +0000

    openstack app changes

---
+++ app_1.py
@@ -0,0 +1,19 @@
+# your app.py
+import os
+import requests
+
+from flask import Flask, g,request, jsonify
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/static/api/1', methods=['GET'])
+def getPhoto():
+    return jsonify({'data': 'success'})
+
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5001)
+
